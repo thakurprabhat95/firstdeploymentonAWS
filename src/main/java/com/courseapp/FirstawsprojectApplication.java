a3ea1961ae03032eeb809288a407e140d9ae38dc
thakurprabhat95 <thakurprabhat95@gmail.com>
2022-01-28 20:58:18 +0530

    first deployemnt demo

---
+++ src/main/java/com/courseapp/FirstawsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.courseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstawsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstawsprojectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/courseapp/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.courseapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	
+	@GetMapping("/demo")
+	public String demo()
+	{
+		return "Hello and welcome to first testing on aws!!!";
+	}
+
+}
